1f148de97b6839f6ba385f6513d38f178086deea
Isulew <224964+netcookies@users.noreply.github.com>
2025-07-10 16:58:31 +0800

    Update ql_env_upload.js

--- jd/ql_env_upload.js
+++ jd/ql_env_upload.js
@@ -16,7 +16,7 @@ try {
 
 async function getScriptUrl() {
   const response = await $.http.get({
-    url: 'https://raw.githubusercontent.com/dompling/Script/master/jd/ql_api.js',
+    url: 'https://raw.githubusercontent.com/netcookies/Script/master/jd/ql_api.js',
   });
   return response.body;
 }
